183c051bd617edf5dec6e8e337784deb7e16c02f
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-11 01:03:54 -0300

    Estilizacao do dashboard - parte 2

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repos } from './styles';
 import logo from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
@@ -13,6 +14,20 @@ export const Dashboard: React.FC = () => {
         <input placeholder="username/repository_name" />
         <button type="submit">Buscar</button>
       </Form>
+
+      <Repos>
+        <a href="/repositories">
+          <img
+            src="https://avatars.githubusercontent.com/u/68967867?v=4"
+            alt="Repositorio"
+          />
+          <div>
+            <strong>aluiziodeveloper/mini-curso-reactjs</strong>
+            <p>Repositorio do mini curso gratuito de reactjs</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repos>
     </>
   );
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -43,3 +43,53 @@ export const Form = styled.form`
     }
   }
 `;
+
+export const Repos = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(6px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
