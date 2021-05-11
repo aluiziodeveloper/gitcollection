8a81cff4d2ff15c1dad77d10d940380e59fc9459
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-11 00:02:27 -0300

    Estilizacao global com styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Routes } from './routes';
+import { GlobalStyle } from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobalStyle />
+    </>
   );
 };
 
---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+import imgBackground from '../assets/background.svg';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: #f0f0f5 url(${imgBackground}) no-repeat 70% top;
+    -wekit-font-smoothing: antialiased;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 2.5rem 1.25rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
