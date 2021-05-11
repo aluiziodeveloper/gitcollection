b093605790e8ba5d029eba3ecadd71fdab747bad
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-11 01:55:33 -0300

    Estilizacao condicional com base em propriedades

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import { api } from '../../services/api';
-import { Title, Form, Repos } from './styles';
+import { Title, Form, Repos, Error } from './styles';
 import logo from '../../assets/logo.svg';
 
 interface GithubRepository {
@@ -17,6 +17,7 @@ interface GithubRepository {
 export const Dashboard: React.FC = () => {
   const [repos, setRepos] = React.useState<GithubRepository[]>([]);
   const [newRepo, setNewRepo] = React.useState('');
+  const [inputError, setInputError] = React.useState('');
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
     setNewRepo(event.target.value);
@@ -26,6 +27,12 @@ export const Dashboard: React.FC = () => {
     event: React.FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault();
+
+    if (!newRepo) {
+      setInputError('Informe o username/repositório');
+      return;
+    }
+
     const response = await api.get<GithubRepository>(`repos/${newRepo}`);
 
     const repository = response.data;
@@ -39,13 +46,15 @@ export const Dashboard: React.FC = () => {
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do Github</Title>
 
-      <Form onSubmit={handleAddRepo}>
+      <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
         <input
           placeholder="username/repository_name"
           onChange={handleInputChange}
         />
         <button type="submit">Buscar</button>
       </Form>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Repos>
         {repos.map(repository => (
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
   margin-top: 80px;
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
   display: flex;
@@ -23,6 +27,12 @@ export const Form = styled.form`
     border-radius: 5px 0px 0px 5px;
     color: #3a3a3a;
     border-right: 0;
+
+    ${props =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -92,4 +102,10 @@ export const Repos = styled.div`
       color: #cbcbd6;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top: 8px;
 `;
