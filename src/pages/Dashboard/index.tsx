0c21293daa5b3b7e337ab305e16a207f8491764c
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-10 23:39:01 -0300

    Estilizacao com styled-components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Title } from './styles';
+
 export const Dashboard: React.FC = () => {
-  return <h1>Dashbord</h1>;
+  return <Title>Dashbord</Title>;
 };
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
