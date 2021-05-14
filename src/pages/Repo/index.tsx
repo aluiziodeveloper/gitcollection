82339f19dbd7b2723b6d9be6f96d2432b342eb2a
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-14 00:35:58 -0300

    Pagina de detalhes de um Repo - parte 4

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
-import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Header, RepoInfo } from './styles';
+import { Header, RepoInfo, Issues } from './styles';
 import logo from '../../assets/logo.svg';
 
 interface RepositoryParams {
@@ -45,6 +45,17 @@ export const Repo: React.FC = () => {
           </li>
         </ul>
       </RepoInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>f;lkj lkfjdlkfjk ldjsffjdlkfj</strong>
+            <p>sdfkjdslkfjdflkdjsflkdjfdjfksdjf</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
     </>
   );
 };
--- src/pages/Repo/styles.ts
+++ src/pages/Repo/styles.ts
@@ -72,3 +72,43 @@ export const RepoInfo = styled.section`
     }
   }
 `;
+
+export const Issues = styled.div`
+  margin-top: 80px;
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: flex;
+    text-decoration: none;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(6px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
